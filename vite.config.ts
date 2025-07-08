8520afb171c639c1a67f232b0355ec0e58a65d87
Leandro (ubuntu) <rissov.leandro@gmail.com>
2025-07-08 05:47:00 -0300

    fix root url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/me",
+  base: "/me/",
   server: {
     host: "::",
     port: 8080,
