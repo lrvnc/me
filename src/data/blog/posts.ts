d4a4d9d59130a0df3ba2df93763469077f23c82b
Leandro (Zorin) <rissov.leandro@gmail.com>
2026-01-31 22:47:09 +0000

    wip

--- src/data/blog/posts.ts
+++ src/data/blog/posts.ts
@@ -1,13 +1,13 @@
 import { BlogPost } from './types';
 
 export const blogPosts: BlogPost[] = [
-    {
-        id: 1,
-        title: "First Commit",
-        excerpt: "Happy to announce my new webpage!",
-        date: "2026-02-01",
-        tags: [],
-        readTime: "0 min read",
-        image: "/imgs/mit1-zoom.jpg"
-    },
+    // {
+    //     id: 1,
+    //     title: "First Commit",
+    //     excerpt: "Happy to announce my new webpage!",
+    //     date: "2026-02-01",
+    //     tags: [],
+    //     readTime: "0 min read",
+    //     image: "/imgs/mit1-zoom.jpg"
+    // },
 ];
