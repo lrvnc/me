491be1fcb0fab9d0379671acdb655727b1b83a2a
Leandro (Zorin) <rissov.leandro@gmail.com>
2026-02-01 01:46:32 +0000

    First post

--- src/data/blog/posts.ts
+++ src/data/blog/posts.ts
@@ -1,18 +1,18 @@
 import { BlogPost } from './types';
 import { calculateReadingTime } from "@/lib/utils";
-import buildingMyPersonalWebsite from '@/data/blog/content/building-my-personal-website.md?raw';
+import welcomePost from '@/data/blog/content/welcome.md?raw';
 
 export const blogPosts: BlogPost[] = [
     {
         id: 1,
-        title: "Building my Personal Website",
-        slug: "building-my-personal-website",
-        excerpt: "How I built this website using React, Tailwind CSS, and Framer Motion.",
-        content: buildingMyPersonalWebsite,
-        date: "2024-03-20",
-        tags: ["React", "Tailwind", "Framer Motion"],
-        readTime: calculateReadingTime(buildingMyPersonalWebsite), // Dynamic calculation
-        image: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?q=80&w=2940&auto=format&fit=crop"
+        title: "Hello World!",
+        slug: "welcome",
+        excerpt: "That's my first post, and I couldn't be happier! I hope you find this blog useful somehow :)",
+        content: welcomePost,
+        date: "2026-02-01",
+        tags: [],
+        readTime: calculateReadingTime(welcomePost), // Dynamic calculation
+        image: "/imgs/blackhole.png"
     },
     // Add more posts here...
 ];
